b8bb1c527ccce3d9749b6fe9fe02c2d1655154fb
budi.ss <budi.ss@009ACE1013.nu-ace.ad-ins.com>
2014-02-26 18:09:10 +0700

    aaa

---
+++ ProjectTaps/src/adins/ace/Mail/SendMailTls.java
@@ -0,0 +1,48 @@
+package adins.ace.Mail;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class SendMailTls {
+	public SendMailTls(String fromMail, String passwordFromEmail, String toMail, String subject, String contentMail){
+		final String username = fromMail; //isi alamat email
+		final String password = passwordFromEmail; //isi password email
+ 
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+ 
+		Session session = Session.getInstance(props,
+		  new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		  });
+ 
+		try {
+ 
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(username));
+			message.setRecipients(Message.RecipientType.TO,
+				InternetAddress.parse(toMail));
+			message.setSubject(subject);
+			message.setText(contentMail);
+ 
+			Transport.send(message);
+ 
+			System.out.println("Done");
+ 
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
