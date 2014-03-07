ad3451bd854b6817ca685f611bf4e80e155f79c8
farisa.adelia <farisa.adelia@006ACE1013.nu-ace.ad-ins.com>
2014-03-07 11:39:13 +0700

    organization

---
+++ ProjectTaps/src/adins/ace/taps/bean/organization/OrganizationBean.java
@@ -0,0 +1,106 @@
+package adins.ace.taps.bean.organization;
+
+import java.io.Serializable;
+
+public class OrganizationBean implements Serializable {
+	private String organizationCode;
+	private String organizationName;
+	private String headName;
+	private String headDomain;
+	private Integer organizationLevel;
+	private String parentCode;
+	private String flag;
+	private String createdBy;
+	private String createdDate;
+	private String updateBy;
+	private String updateDate;
+
+	public String getOrganizationCode() {
+		return organizationCode;
+	}
+
+	public void setOrganizationCode(String organizationCode) {
+		this.organizationCode = organizationCode;
+	}
+
+	public String getOrganizationName() {
+		return organizationName;
+	}
+
+	public void setOrganizationName(String organizationName) {
+		this.organizationName = organizationName;
+	}
+
+	public String getHeadName() {
+		return headName;
+	}
+
+	public void setHeadName(String headName) {
+		this.headName = headName;
+	}
+
+	public String getHeadDomain() {
+		return headDomain;
+	}
+
+	public void setHeadDomain(String headDomain) {
+		this.headDomain = headDomain;
+	}
+
+	public Integer getOrganizationLevel() {
+		return organizationLevel;
+	}
+
+	public void setOrganizationLevel(Integer organizationLevel) {
+		this.organizationLevel = organizationLevel;
+	}
+
+	public String getParentCode() {
+		return parentCode;
+	}
+
+	public void setParentCode(String parentCode) {
+		this.parentCode = parentCode;
+	}
+
+	public String getFlag() {
+		return flag;
+	}
+
+	public void setFlag(String flag) {
+		this.flag = flag;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public String getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(String createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public String getUpdateBy() {
+		return updateBy;
+	}
+
+	public void setUpdateBy(String updateBy) {
+		this.updateBy = updateBy;
+	}
+
+	public String getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(String updateDate) {
+		this.updateDate = updateDate;
+	}
+
+}
