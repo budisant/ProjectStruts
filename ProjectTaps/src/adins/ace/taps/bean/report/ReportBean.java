9418c4b9a3ea5a0f9263953403bbda6b50dee115
farisa.adelia <farisa.adelia@006ACE1013.nu-ace.ad-ins.com>
2014-03-07 09:39:04 +0700

    Bean Report

---
+++ ProjectTaps/src/adins/ace/taps/bean/report/ReportBean.java
@@ -0,0 +1,214 @@
+package adins.ace.taps.bean.report;
+
+import java.io.Serializable;
+
+public class ReportBean implements Serializable {
+	private Integer reportId;
+	private String organizationCode;
+	private String firstName;
+	private String lastName;
+	private String projectCode;
+	private String assignmentDate;
+	private String assignmentDueDate;
+	private String assignmentApprovedDate;
+	private String activityType;
+	private String description;
+	private String adhoc;
+	private String reportTo;
+	private String assignBy;
+	private String assignTo;
+	private Integer star;
+	private String assignmentType;
+	private String assignmentCategory;
+	private Integer manhour;
+	private String periodeReport;
+	private String createdBy;
+	private String createdDate;
+	private String updateBy;
+	private String updateDate;
+
+	public Integer getReportId() {
+		return reportId;
+	}
+
+	public void setReportId(Integer reportId) {
+		this.reportId = reportId;
+	}
+
+	public String getOrganizationCode() {
+		return organizationCode;
+	}
+
+	public void setOrganizationCode(String organizationCode) {
+		this.organizationCode = organizationCode;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getProjectCode() {
+		return projectCode;
+	}
+
+	public void setProjectCode(String projectCode) {
+		this.projectCode = projectCode;
+	}
+
+	public String getAssignmentDate() {
+		return assignmentDate;
+	}
+
+	public void setAssignmentDate(String assignmentDate) {
+		this.assignmentDate = assignmentDate;
+	}
+
+	public String getAssignmentDueDate() {
+		return assignmentDueDate;
+	}
+
+	public void setAssignmentDueDate(String assignmentDueDate) {
+		this.assignmentDueDate = assignmentDueDate;
+	}
+
+	public String getAssignmentApprovedDate() {
+		return assignmentApprovedDate;
+	}
+
+	public void setAssignmentApprovedDate(String assignmentApprovedDate) {
+		this.assignmentApprovedDate = assignmentApprovedDate;
+	}
+
+	public String getActivityType() {
+		return activityType;
+	}
+
+	public void setActivityType(String activityType) {
+		this.activityType = activityType;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getAdhoc() {
+		return adhoc;
+	}
+
+	public void setAdhoc(String adhoc) {
+		this.adhoc = adhoc;
+	}
+
+	public String getReportTo() {
+		return reportTo;
+	}
+
+	public void setReportTo(String reportTo) {
+		this.reportTo = reportTo;
+	}
+
+	public String getAssignBy() {
+		return assignBy;
+	}
+
+	public void setAssignBy(String assignBy) {
+		this.assignBy = assignBy;
+	}
+
+	public String getAssignTo() {
+		return assignTo;
+	}
+
+	public void setAssignTo(String assignTo) {
+		this.assignTo = assignTo;
+	}
+
+	public Integer getStar() {
+		return star;
+	}
+
+	public void setStar(Integer star) {
+		this.star = star;
+	}
+
+	public String getAssignmentType() {
+		return assignmentType;
+	}
+
+	public void setAssignmentType(String assignmentType) {
+		this.assignmentType = assignmentType;
+	}
+
+	public String getAssignmentCategory() {
+		return assignmentCategory;
+	}
+
+	public void setAssignmentCategory(String assignmentCategory) {
+		this.assignmentCategory = assignmentCategory;
+	}
+
+	public Integer getManhour() {
+		return manhour;
+	}
+
+	public void setManhour(Integer manhour) {
+		this.manhour = manhour;
+	}
+
+	public String getPeriodeReport() {
+		return periodeReport;
+	}
+
+	public void setPeriodeReport(String periodeReport) {
+		this.periodeReport = periodeReport;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public String getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(String createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public String getUpdateBy() {
+		return updateBy;
+	}
+
+	public void setUpdateBy(String updateBy) {
+		this.updateBy = updateBy;
+	}
+
+	public String getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(String updateDate) {
+		this.updateDate = updateDate;
+	}
+
+}
